8ec070613a654fac2e1ef213c19569d81ee30c74
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-05-09 15:51:16 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,22 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q librewolf-bin | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/192x192/apps/librewolf.png
+export DESKTOP=/usr/share/applications/librewolf.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+LD_LIBRARY_PATH=$PWD/AppDir/bin quick-sharun \
+	./AppDir/bin/*          \
+	/usr/lib/libavcodec.so* \
+	/usr/lib/libcanberra.so*
+
+echo 'MOZ_LEGACY_PROFILES=1'        >> ./AppDir/.env
+echo 'MOZ_APP_LAUNCHER=${APPIMAGE}' >> ./AppDir/.env
 
 # Additional changes can be done in between here
 
